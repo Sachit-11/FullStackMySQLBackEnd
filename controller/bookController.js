a8dc994031f4dd507c788fd9e8830777d8a06ec5
Sachit Gupta <97462285+Sachit-11@users.noreply.github.com>
2023-03-13 21:43:58 +0530

    new1 commit

--- controller/bookController.js
+++ controller/bookController.js
@@ -1,6 +1,21 @@
 import { db } from "../index.js";
-import cloudinary from "../utils/cloudinary.js";
+import { v2 as cloudinary } from 'cloudinary';
+import dotenv from "dotenv";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
 import {extname} from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+let __dirname = dirname(__filename);
+__dirname = join(__dirname, "../config/config.env");
+
+dotenv.config({path: __dirname});
+
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_SECRET,
+});
 
 const getAllBooks = (req, res) => {
     const q = "Select * from books";
